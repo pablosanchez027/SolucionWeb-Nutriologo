1529a57bd3f2623f9fe8bab22dc50e68f40e24ee
Alumno <ulsa@iMacD107.local>
2019-03-25 16:44:35 -0700

    Small Fix

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Mascotas | Iniciar Sesión</title>
+  <title>Nutriólogo | Iniciar Sesión</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
@@ -30,7 +30,7 @@
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
-    <b>Tienda</b>Mascotas
+    <b>Consultorio</b> Nutriólogo
   </div>
   <!-- /.login-logo -->
   <div class="login-box-body">
