c06fe0fb6649a2bab8e286cd56781a1a72bbbaaa
Alumno <ulsa@iMacD107.local>
2019-04-03 18:34:33 -0700

    SmallType Edit

--- resources/views/dietas/create.blade.php
+++ resources/views/dietas/create.blade.php
@@ -25,105 +25,112 @@
                 </div>
             </div>
         </div>
+        <div class="form-group text-right">
+            <button class="btn btn-primary" type="submit">Asignar Dieta</button>
+        </div>
     </div>
+
 </div>
 
-<div class="row">
+<form method="POST" action="{{ route('dietas.store') }}">
+    <div class="row">
 
-    <div class="col-md-4">
-        <div class="box">
-            <div class="box-header with-border">
-                <h3 class="box-title">Lunes</h3>
-            </div>
-            <div class="box-body">
-                <div class="form-group">
-                    <label>Desayuno</label>
-                    <input type="text" class="form-control" placeholder="Título del desayuno" required
-                        name="desayuno_lunes">
-                    <textarea class="form-control" rows="6" placeholder="Descripción del desayuno"
-                        name="descripcion_desayuno_lunes" style="resize: none;"></textarea>
-                </div>
-                <div class="form-group">
-                    <label>Comida</label>
-                    <input type="text" class="form-control" placeholder="Título de la comida" required
-                        name="comida_lunes">
-                    <textarea class="form-control" rows="6" placeholder="Descripción de la comida"
-                        name="descripcion_comida_lunes" style="resize: none;"></textarea>
-                </div>
-                <div class="form-group">
-                    <label>Cena</label>
-                    <input type="text" class="form-control" placeholder="Título de la cena" required name="cena_lunes">
-                    <textarea class="form-control" rows="6" placeholder="Descripción de la cena"
-                        name="descripcion_cena_lunes" style="resize: none;"></textarea>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="col-md-4">
-        <div class="box">
-            <div class="box-header with-border">
-                <h3 class="box-title">Martes</h3>
-            </div>
-            <div class="box-body">
-                <div class="form-group">
-                    <label>Desayuno</label>
-                    <input type="text" class="form-control" placeholder="Título del desayuno" required
-                        name="desayuno_martes">
-                    <textarea class="form-control" rows="6" placeholder="Descripción del desayuno"
-                        name="descripcion_desayuno_martes" style="resize: none;"></textarea>
-                </div>
-                <div class="form-group">
-                    <label>Comida</label>
-                    <input type="text" class="form-control" placeholder="Título de la comida" required
-                        name="comida_martes">
-                    <textarea class="form-control" rows="6" placeholder="Descripción de la comida"
-                        name="descripcion_comida_martes" style="resize: none;"></textarea>
-                </div>
-                <div class="form-group">
-                    <label>Cena</label>
-                    <input type="text" class="form-control" placeholder="Título de la cena" required name="cena_martes">
-                    <textarea class="form-control" rows="6" placeholder="Descripción de la cena"
-                        name="descripcion_cena_martes" style="resize: none;"></textarea>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="col-md-4">
-        <div class="box">
-            <div class="box-header with-border">
-                <h3 class="box-title">Miércoles</h3>
-            </div>
-            <div class="box-body">
-                <div class="form-group">
-                    <label>Desayuno</label>
-                    <input type="text" class="form-control" placeholder="Título del desayuno" required
-                        name="desayuno_miercoles">
-                    <textarea class="form-control" rows="6" placeholder="Descripción del desayuno"
-                        name="descripcion_desayuno_miercoles" style="resize: none;"></textarea>
-                </div>
-                <div class="form-group">
-                    <label>Comida</label>
-                    <input type="text" class="form-control" placeholder="Título de la comida" required
-                        name="comida_miercoles">
-                    <textarea class="form-control" rows="6" placeholder="Descripción de la comida"
-                        name="descripcion_comida_miercoles" style="resize: none;"></textarea>
-                </div>
-                <div class="form-group">
-                    <label>Cena</label>
-                    <input type="text" class="form-control" placeholder="Título de la cena" required
-                        name="cena_miercoles">
-                    <textarea class="form-control" rows="6" placeholder="Descripción de la cena"
-                        name="descripcion_cena_miercoles" style="resize: none;"></textarea>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="col-md-4">
+        <div class="col-md-4">
             <div class="box">
-                <div class="box-header with-border">
+                <div class="box-header with-border text-center">
+                    <h3 class="box-title">Lunes</h3>
+                </div>
+                <div class="box-body">
+                    <div class="form-group">
+                        <label>Desayuno</label>
+                        <input type="text" class="form-control" placeholder="Título del desayuno" required
+                            name="desayuno_lunes">
+                        <textarea class="form-control" rows="6" placeholder="Descripción del desayuno"
+                            name="descripcion_desayuno_lunes" style="resize: none;"></textarea>
+                    </div>
+                    <div class="form-group">
+                        <label>Comida</label>
+                        <input type="text" class="form-control" placeholder="Título de la comida" required
+                            name="comida_lunes">
+                        <textarea class="form-control" rows="6" placeholder="Descripción de la comida"
+                            name="descripcion_comida_lunes" style="resize: none;"></textarea>
+                    </div>
+                    <div class="form-group">
+                        <label>Cena</label>
+                        <input type="text" class="form-control" placeholder="Título de la cena" required
+                            name="cena_lunes">
+                        <textarea class="form-control" rows="6" placeholder="Descripción de la cena"
+                            name="descripcion_cena_lunes" style="resize: none;"></textarea>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="box">
+                <div class="box-header with-border text-center">
+                    <h3 class="box-title">Martes</h3>
+                </div>
+                <div class="box-body">
+                    <div class="form-group">
+                        <label>Desayuno</label>
+                        <input type="text" class="form-control" placeholder="Título del desayuno" required
+                            name="desayuno_martes">
+                        <textarea class="form-control" rows="6" placeholder="Descripción del desayuno"
+                            name="descripcion_desayuno_martes" style="resize: none;"></textarea>
+                    </div>
+                    <div class="form-group">
+                        <label>Comida</label>
+                        <input type="text" class="form-control" placeholder="Título de la comida" required
+                            name="comida_martes">
+                        <textarea class="form-control" rows="6" placeholder="Descripción de la comida"
+                            name="descripcion_comida_martes" style="resize: none;"></textarea>
+                    </div>
+                    <div class="form-group">
+                        <label>Cena</label>
+                        <input type="text" class="form-control" placeholder="Título de la cena" required
+                            name="cena_martes">
+                        <textarea class="form-control" rows="6" placeholder="Descripción de la cena"
+                            name="descripcion_cena_martes" style="resize: none;"></textarea>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="box">
+                <div class="box-header with-border text-center">
+                    <h3 class="box-title">Miércoles</h3>
+                </div>
+                <div class="box-body">
+                    <div class="form-group">
+                        <label>Desayuno</label>
+                        <input type="text" class="form-control" placeholder="Título del desayuno" required
+                            name="desayuno_miercoles">
+                        <textarea class="form-control" rows="6" placeholder="Descripción del desayuno"
+                            name="descripcion_desayuno_miercoles" style="resize: none;"></textarea>
+                    </div>
+                    <div class="form-group">
+                        <label>Comida</label>
+                        <input type="text" class="form-control" placeholder="Título de la comida" required
+                            name="comida_miercoles">
+                        <textarea class="form-control" rows="6" placeholder="Descripción de la comida"
+                            name="descripcion_comida_miercoles" style="resize: none;"></textarea>
+                    </div>
+                    <div class="form-group">
+                        <label>Cena</label>
+                        <input type="text" class="form-control" placeholder="Título de la cena" required
+                            name="cena_miercoles">
+                        <textarea class="form-control" rows="6" placeholder="Descripción de la cena"
+                            name="descripcion_cena_miercoles" style="resize: none;"></textarea>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="box">
+                <div class="box-header with-border text-center">
                     <h3 class="box-title">Jueves</h3>
                 </div>
                 <div class="box-body">
@@ -143,17 +150,18 @@
                     </div>
                     <div class="form-group">
                         <label>Cena</label>
-                        <input type="text" class="form-control" placeholder="Título de la cena" required name="cena_jueves">
+                        <input type="text" class="form-control" placeholder="Título de la cena" required
+                            name="cena_jueves">
                         <textarea class="form-control" rows="6" placeholder="Descripción de la cena"
                             name="descripcion_cena_jueves" style="resize: none;"></textarea>
                     </div>
                 </div>
             </div>
         </div>
-    
+
         <div class="col-md-4">
             <div class="box">
-                <div class="box-header with-border">
+                <div class="box-header with-border text-center">
                     <h3 class="box-title">Viernes</h3>
                 </div>
                 <div class="box-body">
@@ -173,17 +181,18 @@
                     </div>
                     <div class="form-group">
                         <label>Cena</label>
-                        <input type="text" class="form-control" placeholder="Título de la cena" required name="cena_viernes">
+                        <input type="text" class="form-control" placeholder="Título de la cena" required
+                            name="cena_viernes">
                         <textarea class="form-control" rows="6" placeholder="Descripción de la cena"
                             name="descripcion_cena_viernes" style="resize: none;"></textarea>
                     </div>
                 </div>
             </div>
         </div>
-    
+
         <div class="col-md-4">
             <div class="box">
-                <div class="box-header with-border">
+                <div class="box-header with-border text-center">
                     <h3 class="box-title">Sábado</h3>
                 </div>
                 <div class="box-body">
@@ -213,34 +222,35 @@
         </div>
 
         <div class="col-md-4 col-md-offset-4">
-                <div class="box">
-                    <div class="box-header with-border">
-                        <h3 class="box-title">Domingo</h3>
+            <div class="box">
+                <div class="box-header with-border text-center">
+                    <h3 class="box-title">Domingo</h3>
+                </div>
+                <div class="box-body">
+                    <div class="form-group">
+                        <label>Desayuno</label>
+                        <input type="text" class="form-control" placeholder="Título del desayuno" required
+                            name="desayuno_domingo">
+                        <textarea class="form-control" rows="6" placeholder="Descripción del desayuno"
+                            name="descripcion_desayuno_domingo" style="resize: none;"></textarea>
                     </div>
-                    <div class="box-body">
-                        <div class="form-group">
-                            <label>Desayuno</label>
-                            <input type="text" class="form-control" placeholder="Título del desayuno" required
-                                name="desayuno_domingo">
-                            <textarea class="form-control" rows="6" placeholder="Descripción del desayuno"
-                                name="descripcion_desayuno_domingo" style="resize: none;"></textarea>
-                        </div>
-                        <div class="form-group">
-                            <label>Comida</label>
-                            <input type="text" class="form-control" placeholder="Título de la comida" required
-                                name="comida_domingo">
-                            <textarea class="form-control" rows="6" placeholder="Descripción de la comida"
-                                name="descripcion_comida_domingo" style="resize: none;"></textarea>
-                        </div>
-                        <div class="form-group">
-                            <label>Cena</label>
-                            <input type="text" class="form-control" placeholder="Título de la cena" required
-                                name="cena_domingo">
-                            <textarea class="form-control" rows="6" placeholder="Descripción de la cena"
-                                name="descripcion_cena_domingo" style="resize: none;"></textarea>
-                        </div>
+                    <div class="form-group">
+                        <label>Comida</label>
+                        <input type="text" class="form-control" placeholder="Título de la comida" required
+                            name="comida_domingo">
+                        <textarea class="form-control" rows="6" placeholder="Descripción de la comida"
+                            name="descripcion_comida_domingo" style="resize: none;"></textarea>
+                    </div>
+                    <div class="form-group">
+                        <label>Cena</label>
+                        <input type="text" class="form-control" placeholder="Título de la cena" required
+                            name="cena_domingo">
+                        <textarea class="form-control" rows="6" placeholder="Descripción de la cena"
+                            name="descripcion_cena_domingo" style="resize: none;"></textarea>
                     </div>
                 </div>
             </div>
-</div>
+        </div>
+    </div>
+</form>
 @endsection
